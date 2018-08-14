ca358f22ff5dacd0c2290b15db60cbafa6da4e6b
ekhoshnood <e.khoshnood966@gmail.com>
2018-08-14 23:56:05 +0430

    working before anko

--- app/src/main/java/com/example/a259/sqltest/MainActivity.kt
+++ app/src/main/java/com/example/a259/sqltest/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.a259.sqltest
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import kotlinx.android.synthetic.main.activity_main.*
 import org.jetbrains.anko.toast
 
@@ -10,20 +11,18 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+   }
 
-        saveButton.setOnClickListener({
-            if (nameEditText.text.toString().length > 0 &&
-                    lastNameEditText.text.toString().length > 0 &&
-                    phoneEditText.text.toString().length > 0){
-                var contact = Contact(nameEditText.text.toString(), lastNameEditText.text.toString(),
-                        phoneEditText.text.toString().toInt())
-                var db = DatabaseHandler(this)
-                db.inssrtData(contact)
-            }else{
-                toast("Please fill all fields")
-            }
-        })
-
-
+    fun saveButtonClicked(view: View) {
+        if (nameEditText.text.toString().length > 0 &&
+                lastNameEditText.text.toString().length > 0 &&
+                phoneEditText.text.toString().length > 0){
+            var contact = Contact(nameEditText.text.toString(), lastNameEditText.text.toString(),
+                    phoneEditText.text.toString().toInt())
+            var db = DatabaseHandler(this)
+            db.inssrtData(contact)
+        }else{
+            toast("Please fill all fields")
+        }
     }
 }
